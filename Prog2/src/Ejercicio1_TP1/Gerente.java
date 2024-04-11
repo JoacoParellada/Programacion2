fffeabedf777f724539ca50d44482d5bad2982bf
Joaquin Parellada <joaquiinparellada@gmail.com>
2024-04-11 17:46:17 -0300

    Commit ejercicio1 Trabajo practico 1

---
+++ Prog2/src/Ejercicio1_TP1/Gerente.java
@@ -0,0 +1,22 @@
+package Ejercicio1_TP1;
+
+public class Gerente extends Empleado{
+    private String departamento;
+
+    public Gerente(String nombre, int edad, float salario,Boolean trabajando, String departamento) {
+        super(nombre,edad, salario,trabajando);
+        this.departamento = departamento;
+    }
+    public String getDepartamento() {
+        return departamento;
+    }
+    public void setDepartamento(String departamento) {
+
+    }
+    public void organizarActividades(){
+        System.out.println("Organizando Actividades para el departamento de "+departamento+".");
+    }
+
+
+
+}
---
+++ Prog2/src/Ejercicio1_TP1/Trabajador.java
@@ -0,0 +1,13 @@
+package Ejercicio1_TP1;
+
+public class Trabajador extends Empleado{
+    private String area;
+
+    public Trabajador(String nombre, int edad, float salario,Boolean trabajando, String area) {
+        super(nombre, edad, salario, trabajando);
+        this.area = area;
+    }
+    public void producir(){
+        System.out.println("Trabajador en produccion para el area de "+ area+".");
+    }
+}
